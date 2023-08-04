31ae09007e4ebec52862b6baa36ebcf7f6a85eb5
noble-ops <birunoble@gmail.com>
2023-08-04 21:13:28 +0300

    homepage intial setup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,13 +2,14 @@ import { Container } from "react-bootstrap";
 
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import HomeScreen from "./screens/HomeScreen";
 function App() {
   return (
     <div>
       <Header />
       <main className="py-3">
         <Container>
-          <h1>welcome</h1>
+          <HomeScreen/>
         </Container>
       </main>
       <Footer />
---
+++ frontend/src/components/Product.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+export default function Product({ product }) {
+  return <Card className="my-3 p-3 rounded"></Card>;
+}
