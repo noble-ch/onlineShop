6cd17b2d9761a8611ba0ee6acf29cb05413d1cc9
noble-ops <birunoble@gmail.com>
2023-08-04 20:27:38 +0300

    created navbar,header,main

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "react-bootstrap";
 
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>welcome</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ frontend/src/components/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { Navbar, Nav, Container, Row } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+        <Container fluid>
+          <Navbar.Brand href="/">OTICSHOP</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav
+              className="me-auto my-2 my-lg-0"
+              style={{ maxHeight: "100px" }}
+              navbarScroll>
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
