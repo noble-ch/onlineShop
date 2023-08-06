05b82047110dc23082a72ce3b1b00a0137172710
noble-ch <birunoble@gmail.com>
2023-08-06 19:28:06 +0300

    deleted everything

--- backend/base/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.getRoutes, name="routes"),
-    path('products/', views.getProducts, name="products"),
-    path('products/<str:pk>', views.getProduct, name="product"),
-]
-
