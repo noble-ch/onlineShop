0ce6184e05b1f2c883e7b035d2443e62d485a18c
noble-ch <birunoble@gmail.com>
2023-08-09 11:38:59 +0300

    done with back end

--- base/views.py
+++ base/views.py
@@ -1,3 +1,42 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .products import products
 
 # Create your views here.
+
+
+@api_view(['GET'])
+def getRoutes(request):
+    routes = [
+        '/api/products/',
+        '/api/products/create/',
+
+        '/api/products/<id>/reviews/',
+
+        '/api/products/top/',
+        '/api/products/<id>',
+
+        '/api/products/delete.<id>/',
+        '/api/products/<update>/<id>/',
+
+    ]
+
+    return Response(routes)
+
+
+@api_view(['GET'])
+def getProducts(request):
+    return Response(products)
+
+
+@api_view(['GET'])
+def getProduct(request, pk):
+    product = None
+    for i in products:
+        if i['_id'] == pk:
+            product = i
+            break
+
+    return Response(product)
