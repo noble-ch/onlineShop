cacc43fa8ee608a2269e782a73a941f61eabe131
noble-ch <birunoble@gmail.com>
2023-08-14 23:59:28 +0300

    ddd

--- base/urls.py
+++ base/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
+from base.views import user_views as views
 
 urlpatterns = [
-    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/users/login/', views.MyTokenObtainPairView.as_view(),
+         name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product"),
