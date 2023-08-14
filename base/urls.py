2f3ff917d25330bb359d9ff5aaf327426e641358
noble-ch <birunoble@gmail.com>
2023-08-14 23:56:36 +0300

    configure the path

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('', views.getRoutes, name="routes"),
     path('products/', views.getProducts, name="products"),
     path('products/<str:pk>', views.getProduct, name="product"),
 ]
-
