0a752d61592d4c5ff42c8567640d0c479eccd607
noble-ch <birunoble@gmail.com>
2023-08-16 00:27:09 +0300

    image

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -4,7 +4,7 @@ import { LinkContainer } from "react-router-bootstrap";
 function Header() {
   return (
     <header>
-      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
+      <Navbar bg="light" variant="light" expand="lg" collapseOnSelect>
         <Container fluid>
           <LinkContainer to="/">
             <Navbar.Brand>OTICSHOP</Navbar.Brand>
