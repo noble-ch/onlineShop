84c5bb63c3e6a438d7ff54a921aaaedbbe71ed92
noble-ch <birunoble@gmail.com>
2023-08-18 00:34:18 +0300

    login screen no route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,6 +5,7 @@ import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
 import ProductScreen from "./screens/ProductScreen";
 import CartScreen from "./screens/CartScreen";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} />
+            <Route path="/login" component={LoginScreen} />
             <Route path="/product/:id" element={<ProductScreen />} />
             <Route path="/cart/:id?" element={<CartScreen />} />
           </Routes>
---
+++ frontend/src/components/FormContainer.jsx
@@ -0,0 +1,15 @@
+import { Container, Row, Col } from "react-bootstrap";
+import React from "react";
+
+function FormContainer({ children }) {
+  return (
+    <Container>
+      <Row className="justify-content-md-center">
+        <Col xs={12} md={6}>
+          {children}
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+export default FormContainer;
